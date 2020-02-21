43951cf21501de45dbec459f08e50f91f76a4343
MrLucasFischer <lucasfischerpt@gmail.com>
2020-02-21 18:17:42 +0000

    working on getting BERT embeddings

--- code/get_bert_embeddings.py
+++ code/get_bert_embeddings.py
@@ -10,7 +10,7 @@ import pandas as pd
 from scipy import sparse
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-
+from nltk.tokenize import word_tokenize
 
 # loading the dataset
 df = pd.read_csv("../data/Clothing_Shoes_and_Jewelry_5.csv")
@@ -19,10 +19,6 @@ df = pd.read_csv("../data/Clothing_Shoes_and_Jewelry_5.csv")
 df.drop(["unixReviewTime", "asin", "overall", "reviewTime", "reviewerID",
 "reviewerName"], axis=1, inplace=True)
 
-# This cell takes some time (around 1m30s) because in order to clean the sentence, remove the stopwords and stem words
-# Maybe a better solution would be needed for larger texts
-
-# Replace NaN cells with empty strings
 df = df.fillna("")
 
 def clean_sentence(s):
@@ -34,19 +30,15 @@ def clean_sentence(s):
             
         Returns: The input string stripped out of punctuation and stopwords
     """
-
-    # Specify the non-ascii characters. Non ascii chars go from 0-31 and from 128-256
     ascii_ranges = itertools.chain(range(32), range(128, 257))
 
     # Create a lookup table specifying that non-ascii chars should be translated to empty strings
     # This table will be fed to str.translate
     # str.translate uses C-level table lookup so it's much faster than iterating over every char and replacing them
     tbl = dict.fromkeys(ascii_ranges, u"")
-
-    # Create the english stop_words set
+    
     stop_words = set(stopwords.words('english'))
 
-    # instantiate the PorterStemmer object
     porter = PorterStemmer()
 
     s = s.lower().translate(tbl) # Set the text to lower case and remove non-ascii chars
@@ -62,13 +54,13 @@ df["summary"] = df["summary"].apply(clean_sentence)
 # Evaluating the string representation of the list to convert it to an actual list
 df["helpful"] = df["helpful"].apply(lambda str_list: ast.literal_eval(str_list))
 
-# Split the helpful column into num_yes_votes and num_votes
 # Splitting this way is faster the applying a fuction to every row to get the first and second element
 df[['num_yes_votes','num_votes']] = pd.DataFrame(df["helpful"].values.tolist(), index= df.index)
 
-# Creating the prop_yes_votes columns. Where there is no votes just leave at zero (avoid dividing by zero)
+# Avoid dividing by zero with np.where
 df["prop_yes_votes"] = np.where(df["num_votes"] == 0, 0, df["num_yes_votes"] / df["num_votes"])
 
+# Creating the binary target label indicating if a review was helpful or not
 df["helpful_review"] = ((df["num_votes"] >= 4) & (df["prop_yes_votes"] >= 0.5)).astype(int)
 
 df.drop(["prop_yes_votes", "num_yes_votes", "num_votes", "helpful"], axis=1, inplace=True)
@@ -107,4 +99,11 @@ df_encoded.insert(loc=0, column='original_summary', value=df["summary"])
 df_encoded.insert(loc=0, column='helpful_review', value=df["helpful_review"])
 
 # Download the encoded csv
-df_encoded.to_csv("./output/spam_encoded.csv", index=False)
+df_encoded.to_csv("./bert_encoded_summary.csv", index=False)
+
+
+
+
+
+
+####### CANT LOAD IT INTO MEMORY, JUST TAKE A SMALL SAMPLE LIKE 5% OR SOMETHING LIKE THAT
